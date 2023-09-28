c903c2ef0d4368b0036e937784255bae6d15ab65
timbilding <144000698+timbilding@users.noreply.github.com>
2023-09-28 09:46:46 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите путь к папке:");
+            string Folder = Console.ReadLine();
+
+            Console.WriteLine("Введите ключевое слово для поиска:");
+            string WordSearch = Console.ReadLine();
+
+            string[] files = Directory.GetFiles(Folder, "*", SearchOption.AllDirectories);
+
+            Console.WriteLine("Результаты поиска:");
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+
+                if (fileName.Contains(WordSearch))
+                {
+                    Console.WriteLine(fileName);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
